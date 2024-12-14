e0a2669053c3865c71ce43fcc5c00dfcaa2d5e95
MunkhtTsolmon <monkhtsolmonmuzo@gmail.com>
2024-12-14 14:31:04 +0800

    kk

--- src/app/card.jsx
+++ src/app/card.jsx
@@ -1,34 +1,126 @@
 import { CiLocationOn } from "react-icons/ci";
 import Image from "next/image";
-export function Card({ className, value }) {
+
+export function Card({ value, city, weather }) {
+  // Day and Night Background Color
   const nightColor =
     "bg-gradient-to-t from-[#111827] from-0% to-[#1F2937] to-200%";
   const dayColor =
     "bg-gradient-to-t from-[#F9FAFB00] from-200% to-[#F9FAFB] to-0%";
   const marginTopNight = "mt-[13.5rem]";
-  const marginTopDay = "mt-[7rem]";
+  const marginTopDay = "mt-[6rem]";
   const color = value === "night" ? nightColor : dayColor;
   const marginTop = value === "night" ? marginTopNight : marginTopDay;
-  const imgNightMoon = "/nightMoon.png";
+
   const imgSun = "/Sun.png";
-  const imgSource = value === "night" ? imgNightMoon : imgSun;
+  const imgRain = "/Rain.png";
+  const imgStorm = "/storm.png";
+  const imgSnow = "/snow.png";
+  const imgWind = "/Wind.png";
+  const imgCloud = "/Clouds.png";
+  const imgNightRain = "/nightRain.png";
+  const imgNightStorm = "/nightStorm.png";
+  const imgNightSnow = "/nightSnow.png";
+  const imgNightWind = "/nightWind.png";
+  const imgNightMoon = "/nightMoon.png";
+  const imgNightCloud = "/nightClouds.png";
+  console.log(imgNightMoon);
+  console.log(imgCloud);
+
+  let imgSource = imgSun;
+  const conditionText = weather.condition;
+
+  if (value == "night") {
+    if (conditionText == "Clear") {
+      imgSource = imgNightMoon;
+    } else if (conditionText == "Partly cloudy") {
+      imgSource = imgNightCloud;
+    } else if (conditionText == "Cloudy" || conditionText == "Overcast ") {
+      imgSource = imgNightCloud;
+    } else if (conditionText == "Patchy rain nearby") {
+      imgSource = imgNightRain;
+    } else if (
+      conditionText == "Rain showers" ||
+      conditionText == "Light rain" ||
+      conditionText == "Moderate rain" ||
+      conditionText == "Heavy rain"
+    ) {
+      imgSource = imgNightRain;
+    } else if (conditionText == "Thunderstorm") {
+      imgSource = imgNightStorm;
+    } else if (
+      conditionText == "Snow showers" ||
+      conditionText == "Light snow" ||
+      conditionText == "Moderate snow" ||
+      conditionText == "Heavy snow"
+    ) {
+      imgSource = imgNightSnow;
+    } else if (conditionText == "Sleet") {
+      imgSource = imgNightSnow;
+    } else if (conditionText == "Hail") {
+      imgSource = imgNightStorm;
+    } else if (conditionText == "Windy" || conditionText == "Blustery") {
+      imgSource = imgNightWind;
+    } else if (conditionText == "Mist" || conditionText == "Fog") {
+      imgSource = imgNightCloud;
+    }
+  } else {
+    if (conditionText == "Clear") {
+      imgSource = imgSun;
+    } else if (conditionText == "Partly Cloudy ") {
+      imgSource = imgCloud;
+    } else if (conditionText == "Cloudy" || conditionText == "Overcast ") {
+      imgSource = imgCloud;
+    } else if (conditionText == "Patchy rain nearby") {
+      imgSource = imgRain;
+    } else if (
+      conditionText == "Rain showers" ||
+      conditionText == "Light rain" ||
+      conditionText == "Moderate rain" ||
+      conditionText == "Heavy rain"
+    ) {
+      imgSource = imgRain;
+    } else if (conditionText == "Thunderstorm") {
+      imgSource = imgStorm;
+    } else if (
+      conditionText == "Snow showers" ||
+      conditionText == "Light snow" ||
+      conditionText == "Moderate snow" ||
+      conditionText == "Heavy snow"
+    ) {
+      imgSource = imgSnow;
+    } else if (conditionText == "Sleet") {
+      imgSource = imgSnow;
+    } else if (conditionText == "Hail") {
+      imgSource = imgStorm;
+    } else if (conditionText == "Windy" || conditionText == "Blustery") {
+      imgSource = imgWind;
+    } else if (conditionText == "Mist" || conditionText == "Fog") {
+      imgSource = imgCloud;
+    }
+  }
+
   const cityTextDay = "text-[#111827]";
   const cityTextNight = "text-[#fff]";
   const cityTextColor = value === "night" ? cityTextNight : cityTextDay;
+
   const degreeTextColorNight =
     "bg-gradient-to-t from-[#F9FAFB00] from-0% to-[#F9FAFB] to-100%";
   const degreeTextColorDay =
     "bg-gradient-to-t from-[#6B7280] from-0% to-[#111827] to-600%";
   const degreetextColor =
     value === "night" ? degreeTextColorNight : degreeTextColorDay;
+
   const moodTextColorNight = "text-[#777CCE]";
   const moodTextColorDay = "text-[#FF8E27]";
   const moodTextColor =
     value === "night" ? moodTextColorNight : moodTextColorDay;
+
   const locationColorDay = "opacity-50";
   const locationColorNight = "text-[#fff] opacity-50";
   const locationColor =
     value === "night" ? locationColorNight : locationColorDay;
+
   return (
     <div
       className={`w-[26rem] h-[52rem] ${color} mx-auto ${marginTop} rounded-[2rem] bg-blur-[200px] absolute left-[12rem] z-[10] `}
@@ -38,7 +130,7 @@ export function Card({ className, value }) {
           September 10, 2021
         </p>
         <h1 className={`${cityTextColor} text-[48px] font-extrabold`}>
-          Krakow
+          {city}
         </h1>
       </div>
       <CiLocationOn
@@ -48,17 +140,17 @@ export function Card({ className, value }) {
         className="mx-auto mt-[40px] drop-shadow-[0px_15px_45px_rgba(255,255,255,0.3)]"
         width={265}
         height={265}
-        alt="night-moon"
+        alt="Weather condition"
         src={imgSource}
       />
       <div className="pl-10">
         <h1
-          className={`font-extrabold text-[144px] leading-[197px] ${degreetextColor} inline-block text-transparent bg-clip-text`}
+          className={`font-extrabold text-[8.5rem] leading-[197px] ${degreetextColor} inline-block text-transparent bg-clip-text`}
         >
-          17°
+          {weather.tempDay}°
         </h1>
         <h4 className={`${moodTextColor} text-[24px] font-extrabold `}>
-          Clear
+          {weather.condition}
         </h4>
       </div>
     </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,63 @@
+"use client";
+import { useEffect, useState } from "react";
 import { Card } from "./card.jsx";
 import { SearchInput } from "./searchInput.jsx";
 
+const API_KEY = "eb7323f19ada4471b0072607241312";
+
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState("London");
+  const [dayWeather, setDayWeather] = useState({
+    tempDay: 0,
+    condition: "",
+  });
+  const [nightWeather, setNightWeather] = useState({
+    tempDay: 0,
+    condition: "",
+  });
+  const onChangeText = (e) => {
+    setSearch(e.target.value);
+  };
+  const onKeyDown = (event) => {
+    if (event.code === "Enter") {
+      setCity(search);
+    }
+  };
+
+  useEffect(() => {
+    fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=1&aqi=no&alerts=no`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // console.log(data.forecast.forecastday[0].day.maxtemp_c);
+        // console.log(data.forecast.forecastday[0].day.condition.text);
+        console.log();
+        setDayWeather({
+          tempDay: data.forecast.forecastday[0].day.maxtemp_c,
+          condition: data.forecast.forecastday[0].day.condition.text,
+        });
+        setNightWeather({
+          tempDay: data.forecast.forecastday[0].day.mintemp_c,
+          condition: data.forecast.forecastday[0].hour[0].condition.text,
+        });
+      });
+  }, [city]);
+  console.log(city);
   return (
     <div className="w-[100rem] flex m-auto bg-white relative justify-center items-center overflow-hidden">
       <div className="w-[50rem] h-[75rem] bg-[#F3F4F6]">
-        <SearchInput />
-        <Card />
+        <SearchInput
+          search={search}
+          onChangeText={onChangeText}
+          onKeyDown={onKeyDown}
+        />
+        <Card weather={dayWeather} city={city} />
       </div>
       <div className="w-[50rem] h-[75rem] rounded-lg relative bg-[#F3F4F6]">
-        <Card value="night" />
+        <Card weather={nightWeather} city={city} value="night" />
 
         <div className="w-full h-[550px] rounded-bl-[80px] bg-[#0F141E]"></div>
         <div className="w-[46.89rem] h-[100px] ml-[50px] bg-[#0F141E]"></div>
@@ -20,11 +68,11 @@ export default function Home() {
       <div className="rounded-full size-[145px] absolute z-[3] bg-[#F3F4F6]"></div>
       <div className="rounded-tl-full rounded-bl-full rounded-r-none  border h-[147px] w-[77px] mr-[70px] absolute"></div>
 
-      <div className="size-[340px] rounded-full   absolute border"></div>
-      <div className="size-[540px] rounded-full   absolute border"></div>
-      <div className="size-[940px] rounded-full   absolute border"></div>
-      <div className="size-[1340px] rounded-full   absolute border"></div>
-      <div className="size-[1740px] rounded-full   absolute border"></div>
+      <div className="size-[340px] rounded-full  opacity-[12%] absolute border-[#535353] border-[1px]"></div>
+      <div className="size-[540px] rounded-full  opacity-[12%] absolute border-[#535353] border-[1px]"></div>
+      <div className="size-[940px] rounded-full  opacity-[12%]  absolute border-[#535353] border-[1px]"></div>
+      <div className="size-[1340px] rounded-full opacity-[12%] absolute border-[#535353] border-[1px]"></div>
+      <div className="size-[1740px] rounded-full opacity-[12%] absolute border-[#535353] border-[1px]"></div>
     </div>
   );
 }
